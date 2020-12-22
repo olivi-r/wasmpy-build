26aa2de752ce92afc4dbfbaaeb33306daef9a32a
Jay Ryan <jay.p.ryan.2003@gmail.com>
2020-12-22 20:47:23 +0000

    Added build script

---
+++ wasmpy-build/__init__.py
@@ -0,0 +1,27 @@
+import os, platform, subprocess, sys
+
+
+def build(command):
+    command = command[1:]
+
+    # find cpython include files
+    include_dir = os.path.join(os.path.dirname(__file__), "include", "cp")
+    version = "".join(str(i) for i in sys.version_info[:2])
+    include_dir += version
+
+    emcc = "emcc.bat" if platform.system() == "Windows" else "emcc"
+    args = [
+        emcc, "-Wno-visibility", f"-I{include_dir}",
+        "-sSIDE_MODULE", "-sSTANDALONE_WASM", "-DSIZEOF_WCHAR_T",
+    ]
+    try:
+        subprocess.call(args + command)
+
+    except FileNotFoundError:
+        print("emsdk not found\nYou can install it from https://github.com/ems\
+cripten-core/emsdk\nIf it is already installed make sure to call the emsdk_env\
+.sh/emsdk_env.bat script or add emsdk to path.")
+
+
+if __name__ == "__main__":
+    build(sys.argv)
