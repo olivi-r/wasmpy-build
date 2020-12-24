cff91dd9e693e22f10cb256c389a177e51648a19
James Ryan <r.james.dev@gmail.com>
2020-12-24 17:29:36 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def recurse_files(directory):
 
 setuptools.setup(
     name="wasmpy-build",
-    version="0.1.1",
+    version="0.1.2",
     author="James Ryan",
     author_email="r.james.dev@gmail.com",
     description="Emscripten compatible build script for CPython C extensions",
