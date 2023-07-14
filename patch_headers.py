6745e75e49bc019a557f5a832aee06cd47e43702
Olivia Ryan <olivia.r.dev@gmail.com>
2023-07-15 00:43:07 +0100

    Add pre-generated manually maintained pyconfig.h for reduced build times

--- patch_headers.py
+++ patch_headers.py
@@ -7,13 +7,10 @@ for patch_file in os.listdir("patches"):
     ver_tag = "cp" + "".join(ver.split("."))
 
     subprocess.check_call(["git", "-C", "cpython", "checkout", ver])
-    os.chdir(os.path.join(os.path.dirname(__file__), "cpython"))
-    subprocess.check_call(["./configure"])
-    os.chdir(os.path.dirname(__file__))
 
     # copy headers and license
     shutil.copytree("cpython/Include", f"wasmpy_build/include/{ver_tag}")
-    shutil.copy2("cpython/pyconfig.h", f"wasmpy_build/include/{ver_tag}/pyconfig.h")
+    shutil.copy2("pyconfig.h", f"wasmpy_build/include/{ver_tag}/pyconfig.h")
     shutil.copy2("cpython/LICENSE", f"wasmpy_build/include/{ver_tag}/LICENSE")
 
     # patch headers
